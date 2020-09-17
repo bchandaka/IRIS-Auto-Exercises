77717e4c3f2b28e11017886f8a117b1559adaeb6
Bhargav Chandaka <bhargav2900@gmail.com>
2020-09-17 16:42:16 -0500

    Added Solutions

--- Simple Obstacle Avoidance/main.py
+++ Simple Obstacle Avoidance/main.py
@@ -1,12 +1,53 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("obstacle_scene_1.jpg")
+img = cv2.imread("images/scene2.jpg")
 
 cv2.imshow("Original", img)
-# TODO: Write obstacle avoidance code here...
+# Step 1: Edge Detection
+result = cv2.Canny(img, 20, 200)
+cv2.imshow("Edges", result)
 
+# Step 2: BottomFill
+height, width, channels = img.shape
+print(height, width)
+for col in range(width):
+    for row in range(height-1, 0, -1):
+        if (result[row, col] == 255):
+            break
+        result[row,col] = 255
+cv2.imshow("BottomFilled", result)
 
+# Step 3: Erosion
+kernel = np.ones((30,30),np.uint8)
+result = cv2.erode(result, kernel, iterations=1)
+cv2.imshow("Eroded", result)
+
+# Step 4: Find max safe point
+maxPoint = (0, height-1)    # bottom row, first column
+for col in range(width):
+    for row in range(height-1, 0, -1):
+        if (result[row, col] == 0):
+            break
+        if row < maxPoint[1]:
+            maxPoint = (col, row)
+
+# Step 5: Draw the maxpoint on the original
+center_coordinates = maxPoint
+radius = 5
+color = (255, 0, 0) # BLue in BGR
+thickness = 2
+result = cv2.circle(img, center_coordinates, radius, color, thickness)
+cv2.imshow("Max Point", result)
+
+# Step 6: Choose robot direction
+x = maxPoint[0]
+if (x < width/3):
+    print("Move to the Left!")
+elif (x > (2*width)/3):
+    print("Move to the Right!")
+else:
+    print("Move straight forward!")
 
 #waits for user to press any key 
 # (this is necessary to avoid Python from stopping) 
